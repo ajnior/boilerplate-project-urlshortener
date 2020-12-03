c25735c84b82f7f749dc6e3097b5cbdf6c72628c
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2020-12-03 08:31:22 -0300

    feat: connects with mongoDB Atlas on startup

---
+++ db/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connectMongoDB = async () => {
+  await mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("Connected to mongoDB...");
+};
+
+const connectDB = () => {
+  try {
+    connectMongoDB();
+  } catch (e) {
+    console.log("Problem connecting with mongoDB", e);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 const shorturl = require("./routes/shorturl");
+const connectDB = require("./db/index");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+connectDB();
 
 app.use(cors());
 
